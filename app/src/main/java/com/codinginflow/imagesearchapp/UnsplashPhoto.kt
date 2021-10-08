826b875cf39d33bf0f21826b8a9c0dfcb792fb54
antongordeev <antongordeev9505@gmail.com>
2021-10-08 11:17:13 +0300

    Created object we get from Api in Json type. Created layouts

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/UnsplashPhoto.kt
@@ -0,0 +1,31 @@
+package com.codinginflow.imagesearchapp
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+//parcelize - the way to pass parcelable object between activities or fragments
+@Parcelize
+data class UnsplashPhoto(
+    val description: String?,
+    val id: String,
+    val urls: UnsplashPhotoUrls,
+    val user: UnsplashUser
+): Parcelable {
+
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val full: String,
+        val raw: String,
+        val regular: String,
+        val small: String,
+        val thumb: String
+    ): Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val name: String,
+        val username: String
+    ): Parcelable {
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=ImageSearchApp&utm_medium=referral"
+    }
+}
