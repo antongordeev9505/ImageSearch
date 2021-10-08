04054c3b87f815a65ae6defb1585103801eb1d35
antongordeev <antongordeev9505@gmail.com>
2021-10-08 11:43:52 +0300

    Created GalleryFragment.kt and nav_graph.xml. Implemented Navigation Component

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.codinginflow.imagesearchapp.R
+
+//inflate layout here in constructor
+class GalleryFragment: Fragment(R.layout.fragment_gallery){
+}
