c8b50a975837a54139d75417ca5bd8a3b52093b0
antongordeev <antongordeev9505@gmail.com>
2021-10-10 13:31:07 +0300

    Use nav args to fill DetailsFragment.kt with photo. Use Glide to listen the end of loading of image. Use implicit intent. Up button using Nav controller.

--- app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
@@ -2,13 +2,39 @@ package com.codinginflow.imagesearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 //mainActivity should be injected themself
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    //make Upbutton up
+    //NavController - knows how to handle navigation in NavHost (navHost is MainActivity)
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //if we use FragmentContainerView - use this... insted of just findNavController
+        //reference to navhost
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        //connect appBar to Navigation graph
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        //setup Action Bar - to have UpButton
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    //Up Button
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/codinginflow/imagesearchapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/details/DetailsFragment.kt
@@ -1,7 +1,90 @@
 package com.codinginflow.imagesearchapp.ui.details
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.codinginflow.imagesearchapp.R
+import com.codinginflow.imagesearchapp.databinding.FragmentDetailsBinding
 
 class DetailsFragment: Fragment(R.layout.fragment_details) {
+    //we don`t need VM here, cus we have args
+    //and we don`t need to hold up some states and we don`t have sny methods which call to the Repo
+
+    //variable contains our nav args
+    //DetailsFragmentArgs is generated
+    private val args by navArgs<DetailsFragmentArgs>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //access our views in type safe way
+        val binding = FragmentDetailsBinding.bind(view)
+        binding.apply {
+            val photo = args.photo
+
+            //use fragment/activity as a context is more efficient than view`s context
+            Glide.with(this@DetailsFragment)
+                .load(photo.urls.full)
+                .error(R.drawable.ic_error)
+                    //listen the moment when image (Drawable) will be displayed to the screen
+                .listener(object : RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        //in this case placeholder will be displayed
+                        progressBar.isVisible = false
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        textViewCreator.isVisible = true
+                        //cuz descriptions might be null (from Api)
+                        textViewDescription.isVisible = photo.description != null
+                        return false //otherwise we won`t see the image
+
+                    }
+
+                })
+                .into(imageView)
+
+            textViewDescription.text = photo.description
+
+            //uri - address to the image
+            val uri = Uri.parse(photo.user.attributionUrl)
+            //ACTION_VIEW - let the system pick appropriate action to view this (uri) content
+            //it`s implicit intent
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            val nameText = "Photo by ${photo.user.name} on Unsplash"
+
+            textViewCreator.apply {
+                text = nameText
+                //make the text clickable
+                setOnClickListener {
+                    context.startActivity(intent)
+                }
+                //make the text is underlined
+                paint.isUnderlineText = true
+            }
+        }
+    }
 }
