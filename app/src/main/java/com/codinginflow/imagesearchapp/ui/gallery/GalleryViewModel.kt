2feda8a131416a3501e0677ce6e5535b648ede81
antongordeev <antongordeev9505@gmail.com>
2021-10-10 13:54:47 +0300

    Save a piece of data and handle process death

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -1,20 +1,23 @@
 package com.codinginflow.imagesearchapp.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.codinginflow.imagesearchapp.data.UnsplashRepository
 
 //ViewModelInject - the same Injects but for VM
 class GalleryViewModel @ViewModelInject constructor(
-    private val repository: UnsplashRepository
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
 ) : ViewModel() {
 
+    //SavedStateHandle - let us save piece of data throw proccess death
+    //Assisted - let dagger inject new variable
+
     //variable for query that user will type in
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    //currentQuery will be persistant throw proc death
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     //we will observe photos in fragment
     val photos = currentQuery.switchMap { queryString ->
@@ -30,6 +33,8 @@ class GalleryViewModel @ViewModelInject constructor(
     }
 
     companion object {
+        //key for persisting data throw proc death
+        private const val CURRENT_QUERY = "current_query_key"
         //create default query
         private const val DEFAULT_QUERY = "cats"
     }
